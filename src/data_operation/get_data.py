dea321ee7e8861f2ce0e1bb55c16371f6c7c8efc
RealTonyShaw <tonyshaw123@hotmail.com>
2020-08-02 16:54:30 +0800

    Add protVec input

--- src/data_operation/get_data.py
+++ src/data_operation/get_data.py
@@ -4,11 +4,12 @@ import pandas as pd
 # Paths
 covid_metadata_path = "../../data/metadata.tsv"
 covid_sequences_path = "../../data/alignment.fasta"
+covid_protvec_path = "../../data/protVec_100d_3grams.csv"
 
 
 def get_metadata(metadata_path: str):
     """
-    Get the metadata for different
+    Get the metadata for different strains
     :param metadata_path: The path of a metadata file.
     :return: A dataframe object of metadata.
     """
@@ -24,3 +25,13 @@ def get_sequences(sequences_path: str):
     """
     records = list(SeqIO.parse(sequences_path, "fasta"))
     return records
+
+
+def get_protvec(protvec_path: str):
+    """
+    Get protvec
+    :param protvec_path: The path of a protvec file.
+    :return: A dataframe object of protvec.
+    """
+    protvec = pd.read_csv(protvec_path, "	")
+    return protvec
