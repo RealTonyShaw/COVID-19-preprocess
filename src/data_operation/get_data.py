f32f227f44c236a031f993add6734f2736bbb06c
RealTonyShaw <tonyshaw123@hotmail.com>
2020-08-04 10:38:41 +0800

    Return series in embedding

--- src/data_operation/get_data.py
+++ src/data_operation/get_data.py
@@ -38,19 +38,31 @@ def get_protvec(protvec_path: str) -> pd.DataFrame:
     return protvec
 
 
-def convert_sequence_to_protvec(amino_acids_sequence: str, protvec_path: str) -> pd.DataFrame:
+def convert_sequence_to_protvec(amino_acids_sequence: str, protvec_path: str) -> pd.Series:
     """
     Embed a 3-grams sequence into the protVec.
     :param amino_acids_sequence: A 3-grams sequence of amino acids.
     :param protvec_path: The path of a protVec file.
-    :return: A dataframe object of the embedding protVec.
+    :return: A series object of the embedding protVec.
     """
     trigrams = get_overlapping_trigrams_of_sequence(amino_acids_sequence)
     protvec = get_protvec(protvec_path)
     embedded_trigrams = pd.DataFrame()
     for trigram in trigrams:
         if protvec[protvec['words'] == trigram].empty:
+            # if embedded_trigrams.empty:
+            #     embedded_trigrams = protvec[protvec['words'] == '<unk>'].iloc[:, 1:]
+            # else:
+            #     embedded_trigrams = embedded_trigrams + protvec[protvec['words'] == '<unk>'].iloc[:, 1:]
             embedded_trigrams = pd.concat([embedded_trigrams, protvec[protvec['words'] == '<unk>']])
         else:
+            # if embedded_trigrams.empty:
+            #     embedded_trigrams = protvec[protvec['words'] == trigram].iloc[:, 1:]
+            # else:
+            #     embedded_trigrams = embedded_trigrams + protvec[protvec['words'] == trigram].iloc[:, 1:]
             embedded_trigrams = pd.concat([embedded_trigrams, protvec[protvec['words'] == trigram]])
-    return embedded_trigrams
+    return embedded_trigrams.iloc[:, 1:][:].sum()
+
+"""
+def embed_all_sequences(sequences_path: str, protvec_path: str) -> pd.DataFrame:
+"""
